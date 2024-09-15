0db8f53ff3714d61394a1358df3d93126b43f495
S-via <168160925+S-via@users.noreply.github.com>
2024-09-15 14:23:33 -0700

    syntax error for line parenthesisis from app.post line 34 to line 58

--- server.js
+++ server.js
@@ -22,34 +22,42 @@ app.get('/notes', (req, res) =>
 
 ///// GET API route for /notes should read db.json & return svaed notes as JSON/////
 app.get('/api/notes', (req, res) => {
-    fs.readfile(path.join(__dirname, 'db','db.json'), 'utf8', (err, data) => {
+    fs.readfile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.json(JSON.parse(data))
         }
-    })})
+    })
+})
 
 //// POST API /api/notes 
-app.post('/api/notes',(req,res) => {
-   console.log(`${req.method}request received`);
-   const newNote = req.body;
-   if (title && text){
-    const newNote = {
-        title,
-        text,
-        Newid: uuid(),
-    };
-    const response ={
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method}request received`);
+
+    //// recieve as a new note to save on the req.body 
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            Newid: uuid(),
+        };
+    /* const response ={
         status:'suceess',
         body: newNote,
-    };
-    console.log(response);
-   }
+    } */
 
+        console.log(newNote);}
+
+        fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(JSON.parse(data))
+            }})
+        fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(newNote))
 })
-// recieve as a new note to save on the req.body 
+//// readFile one more time before writing or parse it the write it ?
 /// and add it to db.json 
- fs.writeFile('db.json', JSON.stringify(newNote))
 
 
 
